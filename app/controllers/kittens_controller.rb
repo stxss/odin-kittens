021ad97e1cf36cc7760670bd2bd2abf259bdee12
stxss <93729036+stxss@users.noreply.github.com>
2023-05-12 20:17:41 +0100

    Complete first part of the assignment - HTML

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,19 +1,54 @@
 class KittensController < ApplicationController
   def index
+    @kittens = Kitten.all
   end
 
   def new
+    @kitten = Kitten.new
   end
 
   def create
+    @kitten = Kitten.new(kitten_params)
+
+    if @kitten.save
+      redirect_to @kitten, notice: "Kitten created!"
+    else
+      flash.now[:alert] = "Oops, silly you, check your fields again"
+      flash.now[:alert] = @kitten.errors.full_messages
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
+    @kitten = Kitten.find(params[:id])
   end
 
   def update
+    @kitten = Kitten.find(params[:id])
+
+    if @kitten.update(kitten_params)
+      redirect_to root_path
+    else
+      flash.now[:alert] = "Oops, silly you, check your fields again"
+      flash.now[:alert] = @kitten.errors.full_messages
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def show
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def destroy
+    @kitten = Kitten.find(params[:id])
+    @kitten.destroy
+
+    redirect_to root_path, status: :see_other, notice: "You deleted the kitten!"
+  end
+
+  private
+
+  def kitten_params
+    params.require(:kitten).permit(:name, :age, :cuteness, :softness)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :kittens
-
   root "kittens#index"
+  resources :kittens
 end
